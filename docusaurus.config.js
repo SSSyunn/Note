63ce1cde33243e8f373fbd6bcfb4ab81a8f46385
Thirup2 <register@syunn.cn>
2024-10-30 21:53:03 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/SSSyunn/Note',
             label: 'GitHub',
             position: 'right',
           },
